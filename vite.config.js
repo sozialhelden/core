0083f9978374f1231f92e55bac1b431c25de42f7
Kris Siepert <m@kris.cool>
2025-08-25 10:55:42 +0200

    fix!: remove umd module
    This package only supports ES modules from now on.

--- vite.config.js
+++ vite.config.js
@@ -40,19 +40,13 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/index.ts"),
-      fileName: "index",
+      entry: resolve(__dirname, "./lib/index.ts"),
       name: packageJson.name,
+      formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled into your library
       external: ["react"],
-      output: {
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {
-          react: "react",
-        },
-      },
     },
   },
 });
